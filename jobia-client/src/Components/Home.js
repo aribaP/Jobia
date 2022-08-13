56ef10ec1ef864909bf9066b07c4d1b64846356b
jaweria alam <jaweriaalam23@gmai.com>
2022-08-13 22:12:32 +0500

    home page images and css change

--- jobia-client/src/Components/Home.js
+++ jobia-client/src/Components/Home.js
@@ -6,7 +6,6 @@ import NavBarComponent from "./NavBarComponent";
 export default function Home() {
   return (
     <>
-      {/* <Header /> */}
       <NavBarComponent/>
       
       <section className="home-container">
