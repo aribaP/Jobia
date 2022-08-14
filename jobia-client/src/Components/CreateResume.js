7c1628012c28e1b486a3b581d265450870bc4d5c
jaweria alam <jaweriaalam23@gmai.com>
2022-08-15 02:02:07 +0500

    update resume template and profile css

--- jobia-client/src/Components/CreateResume.js
+++ jobia-client/src/Components/CreateResume.js
@@ -1,317 +1,36 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import Profilee from '../assets/Profile.png';
-import Edit from '../assets/edit.png';
-import Footer from './Footer'
-import NavBarComponent2 from "./NavBarComponent2";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const EditOneJob = () => {
+const CreateResume = ({onChangeStatus, onChangeTabs}) => {
 
-    const navigate = useNavigate();
-    const initialvalues = {
-        jdId: "",
-        jdPosition: "",
-        jdMinimumExperience: "",
-        jdRequiredSkills: "",
-        jdLocation: "",
-        jdCity: ""
-    };
+  return (
+    <div className='padding-20 resume-create-container'>
+      <div style={{ border: '4px solid #5B4F64', borderRadius: '20px', width: '100%' }}></div>
+      <h4 className='padding-30 text-center'>Oops! Looks like you haven’t created any resume</h4>
+      <h5 className='padding-30 text-center padding-top-0'>Create your resume now so that Jobia will find the perfect Job for you!</h5>
+      
+      <div className="crud-resume">
+          <button style={{marginLeft: 5, width:72}} className="btn btn-primary" type="submit" onClick={() => {
+            onChangeStatus('ResumeDisplay')
+            }}>View</button>
+        
+          <button style={{marginLeft: 5}} className="btn btn-success" type="submit" onClick={() => {
+            onChangeStatus('ResumePage')
+              onChangeTabs('CreateResume')
+              
+            }}>Create</button>
 
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
-    const [setJD, setJDDetails] = useState({});
-    const [formValues, setFormValues] = useState(initialvalues);
-
-    let name, value;
-    const handleChange = (e) => {
-        name = e.target.name;
-        value = e.target.value;
-        setFormValues({ ...formValues, [name]: value }); //...=>spread operator
-        console.log("form values", formValues);
-
-    };
+          <button style={{marginLeft: 5, color: 'white'}} className="btn btn-warning" type="submit" onClick={() => {
+            }}>Update</button>
 
 
-    const handleCancel = (e) => {
-        navigate('/organization', { replace: true });
-
-    };
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setIsSubmit(true);
-        setFormErrors(validate(formValues));
-
-        if (formValues.jdCity === "")
-            formValues.jdCity = setJD.jdCity;
-
-        if (formValues.jdLocation === "")
-            formValues.jdLocation = setJD.jdLocation;
-
-        if (formValues.jdPosition === "")
-            formValues.jdPosition = setJD.jdPosition;
-
-        if (formValues.jdMinimumExperience === null)
-            formValues.jdMinimumExperience = setJD.jdMinimumExperience;
-
-        if (formValues.jdRequiredSkills === "")
-            formValues.jdRequiredSkills = setJD.jdRequiredSkills;
-
-        console.log(formValues);
-    };
-
-
-    const postData = async (body) => {
-        const data = {
-            jdPosition: body.jdPosition,
-            jdCity: body.jdCity,
-            jdLocation: body.jdLocation,
-            jdMinimumExperience: body.jdMinimumExperience,
-            jdRequiredSkills: body.jdRequiredSkills,
-        };
-        try {
-            await axios.patch("http://localhost:5000/job-description/update/133", data)
-                .then((response) => {
-                    console.log("Data recieved");
-                    console.log(response.data);
-                    alert("Information saved.");
-                    window.location.reload();
-                })
-
-        } catch (err) {
-            console.log(err);
-        }
-
-    };
-
-    const getData = async () => {
-
-        try {
-            await axios.get("http://localhost:5000/job-description/getone/133")
-                .then((response) => {
-                    console.log("Data recieved");
-                    setJDDetails(response.data);
-                    console.log("JDSET", setJD);
-
-                })
-
-        } catch (err) {
-            console.log(err);
-        }
-
-    };
-
-    useEffect(() => {
-        getData();
-        console.log(formErrors);
-        if (isSubmit && Object.keys(formErrors).length === 0) {
-            console.log("useeffect", setJD);
-            postData(formValues);
-            console.log(formValues);
-        }
-
-    }, [formErrors]);
-
-
-
-    const validate = (values) => {
-        const errors = {};
-
-        return errors;
-    };
-
-
-    return (
-        <>
-            
- {/* <Header /> */}
- <NavBarComponent2/>
-         <section className="update">
-          <div>
-            <h2 className="contactUsHeading">Job Update</h2>
-          </div>
-        </section>
-              <div class="row"  style={{
-          
-          justifyContent: "center",height:"40%"
-          
-        }}>
-
-               
-            <div class="col-10 profile-body-right">
-                    <div style={{
-          marginTop: "80px",marginBottom: "80px", borderRadius: "3px",
-          boxShadow: "-1px 3px 18px 0px rgb(0 0 0 / 75%)",
-          padding:"30px"
-        }}>
-                        <label className="mb-3"> Job Position</label>
-                        <div className='orgIcon'>
-                            <input
-                                type="text"
-                                class="form-control input-Fields"
-                                disabled="true"
-                                id="jdPosition"
-                                name="jdPosition"
-                                value={setJD.jdPosition}
-                            />
-                            <button className='btn btn-small btn-outline-secondary'>
-                                <img src={Edit} alt="" width="30px" height="30px" />
-                            </button>
-
-                        </div>
-
-                        <div className='mb-3'>
-                            <input
-                                type="text"
-                                class="form-control input-Fields"
-                                id="jdPosition"
-                                name="jdPosition"
-                                placeholder="Edit here"
-                                value={formValues.jdPosition}
-                                onChange={handleChange}
-                            />
-                            <div className="formErrors text-danger">
-                                <p>{formErrors.jdPosition}</p>
-                            </div>
-                        </div>
-
-                        <label className="mb-3"> Minimum Experience </label>
-                        <div className='orgIcon'>
-                            <input
-                                type="number"
-                                class="form-control input-Fields"
-                                id="jdMinimumExperience"
-                                name="jdMinimumExperience"
-                                disabled="true"
-                                value={setJD.jdMinimumExperience}
-                            />
-                            <button className='btn btn-small btn-outline-secondary'>
-                                <img src={Edit} alt="" width="30px" height="30px" />
-                            </button>
-                        </div>
-
-                        <div className='mb-3'>
-                            <input
-                                type="number"
-                                class="form-control input-Fields"
-                                id="jdMinimumExperience"
-                                name="jdMinimumExperience"
-                                placeholder="Edit here"
-                                value={formValues.jdMinimumExperience}
-                                onChange={handleChange}
-                            />
-                            <div className="formErrors text-danger">
-                                <p>{formErrors.jdMinimumExperience}</p>
-                            </div>
-                        </div>
-
-
-                        <label className="mb-3"> Required Skills</label>
-                        <div className='orgIcon'>
-                            <textarea
-                                type="text"
-                                name="jdRequiredSkills"
-                                class="form-control input-Fields"
-                                id="jdRequiredSkills"
-                                disabled="true"
-                                value={setJD.jdRequiredSkills}
-                            />
-                            <button className='btn btn-small btn-outline-secondary'>
-                                <img src={Edit} alt="" width="30px" height="30px" />
-                            </button>
-                        </div>
-                        <div>
-                            <input
-                                type="text"
-                                class="form-control input-Fields"
-                                id="jdRequiredSkills"
-                                name="jdRequiredSkills"
-                                placeholder="Edit here"
-                                value={formValues.jdRequiredSkills}
-                                onChange={handleChange}
-                            />
-                            <div className="formErrors text-danger">
-                                <p>{formErrors.jdRequiredSkills}</p>
-                            </div>
-                        </div>
-
-                        <label className="mb-3"> City</label>
-                        <div className='orgIcon'>
-                            <input
-                                type="text"
-                                name="jdCity"
-                                class="form-control input-Fields"
-                                id="jdCity"
-                                disabled="true"
-                                value={setJD.jdCity}
-                            />
-                            <button className='btn btn-small btn-outline-secondary'>
-                                <img src={Edit} alt="" width="30px" height="30px" />
-                            </button>
-                        </div>
-                        <div>
-                            <input
-                                type="text"
-                                class="form-control input-Fields"
-                                id="jdCity"
-                                name="jdCity"
-                                placeholder="Edit here"
-                                value={formValues.jdCity}
-                                onChange={handleChange}
-                            />
-                            <div className="formErrors text-danger">
-                                <p>{formErrors.jdCity}</p>
-                            </div>
-                        </div>
-
-                        <label className="mb-3"> Location </label>
-                        <div className='orgIcon'>
-                            <input
-                                type="text"
-                                name="jdLocation"
-                                class="form-control input-Fields"
-                                id="jdLocation"
-                                disabled="true"
-                                value={setJD.jdLocation}
-                            />
-                            <button className='btn btn-small btn-outline-secondary'>
-                                <img src={Edit} alt="" width="30px" height="30px" />
-                            </button>
-                        </div>
-                        <div>
-                            <input
-                                type="text"
-                                class="form-control input-Fields"
-                                id="jdLocation"
-                                name="jdLocation"
-                                placeholder="Edit here"
-                                value={formValues.jdLocation}
-                                onChange={handleChange}
-                            />
-                            <div className="formErrors text-danger">
-                                <p>{formErrors.jdLocation}</p>
-                            </div>
-                        </div>
-
-
-                       
-                        <form className="d-flex justifyContent width-100">
-            <button className="btn button-style-outline me-2 btn-sm" type="submit"  onClick={handleCancel}>Cancel</button>
-         <button className="btn button-style-full me-2 btn-sm" type="submit" onClick={handleSubmit}>Save</button>
-          
-            </form>
-
-                    </div>
-                </div>
-            </div>
-            <footer>
-
-            <Footer dark={true} />
-      </footer>
-        </>
-    )
+          <button style={{marginLeft: 5}} className="btn btn-danger" type="submit" onClick={() => {
+            onChangeStatus('ResumePage')
+              onChangeTabs('DeleteResume')
+            }}>Delete</button>
+        </div>
+    </div>
+  )
 }
 
-export default EditOneJob
+export default CreateResume
