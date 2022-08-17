b3350bf91cbec4ffb969532d78dc1b2bc203a575
MuhammadSabir160 <52483046+MuhammadSabir160@users.noreply.github.com>
2022-08-17 14:12:21 +0500

    resume update and create

--- jobia-client/src/Components/ExistedResume.js
+++ jobia-client/src/Components/ExistedResume.js
@@ -33,18 +33,19 @@ const CreateResume = ({ onChangeStatus, onChangeTabs }) => {
   return (
     <div className='padding-20 resume-create-container'>
       <div style={{ border: '4px solid #5B4F64', borderRadius: '20px', width: '100%' }}></div>
-      <h4 className='padding-30 text-center'>Oops! Looks like you haven’t created any resume</h4>
-      <h5 className='padding-30 text-center padding-top-0'>Create your resume now so that Jobia will find the perfect Job for you!</h5>
 
-      <div className="crud-resume">
-        
-        <button style={{ marginLeft: 5 }} className="btn btn-success" type="submit" onClick={() => {
-          onChangeStatus('ResumePage')
-          onChangeTabs('CreateResume')
+      <div className="crud-resume" style={{paddingTop:'100px'}}>
+        <Link to="/displayresume" state={{ resId: formValues }}><button style={{ marginLeft: 5, width: 72 }} className="btn btn-primary" type="submit">View</button></Link>
 
-        }}>Create</button>
 
-              </div>
+        <Link to="/update" state={{ resId: formValues }}><button style={{ marginLeft: 5, color: 'white' }} className="btn btn-warning" type="submit" >Update</button></Link>
+
+
+        <button style={{ marginLeft: 5 }} className="btn btn-danger" type="submit" onClick={() => {
+          onChangeStatus('CreateResume')
+          onChangeTabs('DeleteResume')
+        }}>Delete</button>
+      </div>
     </div>
   )
 }
